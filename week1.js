fd400e222cfa61682c9a05c5e21b20408e30b178
Sneha Narayan <snehanarayan@gmail.com>
2013-10-25 15:44:06 -0400

    javascript calculate code

---
+++ week1.js
@@ -0,0 +1,5 @@
+ function calculate(){
+                var input = $('#expression'); //get the box
+                var val = input.val(); //get the value inside the box
+                console.log(val.toUpperCase()); //log it
+            }
